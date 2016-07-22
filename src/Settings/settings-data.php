0e54d0cc9db4a71d462e75efa8131f63cf09c683
David Egan <david@carawebs.com>
2016-07-22 20:25:50 +0100

    Add new sttinegs file, as a PHP array

---
+++ src/Settings/settings-data.php
@@ -0,0 +1,54 @@
+<?php
+$settings = array (
+  'default_page_options' =>
+    array (
+      'slug' => 'carawebs_organiser',
+      'title' => 'Organiser',
+      'page_title' => 'Organiser',
+      'parent' => NULL,
+      'id' => NULL,
+      'capability' => 'manage_options',
+      'icon' => 'dashicons-admin-settings',
+      'position' => 89,
+      'desc' => 'Settings for the post organiser drag-and-drop functionality',
+    ),
+  'tabs' =>
+    array (
+      'general' =>
+        array (
+          'slug' => 'general',
+          'title' => 'Main Settings',
+        ),
+      'contact' =>
+        array (
+          'slug' => 'help',
+          'title' => 'HelpAA',
+        ),
+    ),
+  'fields' =>
+    array (
+      0 => array (
+        'tab' => 'general',
+        'args' =>
+          array (
+            'type' => 'cpt_selector',
+            'name' => 'CPTs',
+            'title' => 'Select Post Types',
+            'desc' => 'Select the post types for which you\'d like to allow sorting',
+            'default' => 0,
+          ),
+      ),
+      1 => array (
+        'tab' => 'help',
+        'args' =>
+          array (
+            'name' => 'help',
+            'title' => 'Main Help',
+            'desc' => NULL,
+            'type' => 'message',
+            'file' => '/templates/message.php',
+          ),
+      ),
+    ),
+);
+return $settings;
